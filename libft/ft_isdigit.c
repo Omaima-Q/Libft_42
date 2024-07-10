0c8ced9fb17d3eb6ce0a88f4f51482193d7116a5
Omaima-Q <oqaroot@student.42abudhabi.ae>
2024-07-10 20:54:30 +0400

    Create ft_isdigit.c

---
+++ libft/ft_isdigit.c
@@ -0,0 +1,16 @@
+#include "libft.h"
+#include <stdio.h>
+
+int ft_isalnum(int c)
+{
+	if (ft_isalpha(c) || ft_isdigit(c))
+		return (1);
+	else
+		return (0);
+}
+
+// #include <stdio.h>
+// int main(void)
+// {
+//     printf("%d", ft_isdigit('l'));
+// }
