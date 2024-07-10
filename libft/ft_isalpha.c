9f203b66864c4922caf4eb1f952954e69669774c
Omaima-Q <oqaroot@student.42abudhabi.ae>
2024-07-10 20:47:48 +0400

    Create  ft_isalpha.c

---
+++ libft/ft_isalpha.c
@@ -0,0 +1,16 @@
+#include "libft.h"
+#include <stdio.h>
+
+int ft_isalpha( int a)
+{
+    if((a >= 'a' && a <= 'z') || (a >= 'A' && a <= 'Z'))
+    return(a);
+    else
+    return(0);
+}
+
+int main(void)
+{
+    printf("The result is : %d", ft_isalpha('D'));
+    return (0);
+}
