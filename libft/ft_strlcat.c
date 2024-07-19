43b9c6153853a7514c5b582f07f2776612475f3c
Omaima-Q <oqaroot@student.42abudhabi.ae>
2024-07-19 18:05:05 +0400

    Update ft_strlcat.c

--- libft/ft_strlcat.c
+++ libft/ft_strlcat.c
@@ -11,87 +11,54 @@
 /* ************************************************************************** */
 
 #include "libft.h"
-#include <stdio.h>
 #include <string.h>
+#include <stdio.h>
 
-// size_t  ft_strlcat(char *dst, const char *src, size_t size)
-// {
-//     size_t i;
-// 	size_t j;
-// 	size_t k;
-
-// 	i = 0;
-// 	while (dst[i] != '\0')
-// 		i++;
-// 	j = 0;
-// 	while (src[j] != '\0' && j < size)
-// 	{
-// 		dst[i + j] = src[j];
-// 		j++;
-// 	}
-// 	dst[i + j] = '\0';
-// 	k = 0;
-// 	while (src[k] != '\0')
-// 		k++;
-// 	return (k + size);
-// }
-
-// int main(void) 
-// {
-//     char s1[50] = "This is s1. ";
-//     char s2[50] = "This is s2.";
-
-//     ft_strlcat(s1, s2, 20);
-//     printf("Final string : %s", s1);
-//     return(0);
-// }
-
-size_t ft_strlcat(char *dst, const char *src, size_t size)
+size_t	ft_strlen(const char *s)
 {
-    size_t i = 0;
-    size_t j = 0;
-    size_t dst_len = 0;
-    size_t src_len = 0;
+	size_t	i;
 
-    // Calculate the length of the destination string
-    while (dst[i] != '\0' && i < size)
-    {
-        i++;
-    }
-    dst_len = i;
-
-    // Calculate the length of the source string
-    while (src[j] != '\0')
-    {
-        j++;
-    }
-    src_len = j;
-
-    // Make sure there is room for null-terminator in dst
-    if (size <= dst_len)
-    {
-        return dst_len + src_len;
-    }
-
-    j = 0;
-    while (src[j] != '\0' && dst_len + j < size - 1)
-    {
-        dst[dst_len + j] = src[j];
-        j++;
-    }
-    dst[dst_len + j] = '\0';
-
-    return dst_len + src_len;
+	i = 0;
+	while (s[i])
+		i++;
+	return (i);
 }
 
-int main(void) 
+size_t	ft_strlcat(char *dst, const char *src, size_t size)
 {
-    char s1[50] = "This is s1. ";
-    char s2[50] = "This is s2.";
+	size_t	i;
+	size_t	j;
+	size_t	dst_len;
+	size_t	src_len;
 
-    size_t result = ft_strlcat(s1, s2, 30);
-    printf("Final string: %s\n", s1);
-    printf("Total length: %zu\n", result);
-    
-    return 0;
+	i = 0;
+	j = 0;
+	dst_len = 0;
+	src_len = 0;
+	if (size == 0)
+		return (ft_strlen(src));
+	dst_len = ft_strlen(dst);
+	src_len = ft_strlen(src);
+	if (size <= dst_len)
+		return (dst_len + src_len);
+	j = 0;
+	while (src[j] != '\0' && dst_len + j < size - 1)
+	{
+		dst[dst_len + j] = src[j];
+		j++;
+	}
+	dst[dst_len + j] = '\0';
+	return (dst_len + src_len);
+}
+
+int	main(void)
+{
+	size_t	result;
+	char	s1[50] = "omaima";
+	char	s2[50] = "Waleed qaroot";
+
+	result = ft_strlcat(s1, s2, 10);
+	printf("Final string: %s\n", s1);
+	printf("Total length: %zu\n", result);
+	return (0);
 }
